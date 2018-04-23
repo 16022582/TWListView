accffbcd6b2f4c7144392a782f329eb720dd1b6f
16022582 <16022582@myrp.edu.sg>
2018-04-23 12:32:12 +0800

    created course class

---
+++ app/src/main/java/rp/tw_listview/Courses.java
@@ -0,0 +1,13 @@
+package rp.tw_listview;
+
+public class Courses {
+    private String years;
+
+    public Courses(String years) {
+        this.years = years;
+    }
+
+    public String getYears() {
+        return years;
+    }
+}
---
+++ app/src/main/java/rp/tw_listview/CoursesAdapter.java
@@ -0,0 +1,4 @@
+package rp.tw_listview;
+
+public class CoursesAdapter {
+}
