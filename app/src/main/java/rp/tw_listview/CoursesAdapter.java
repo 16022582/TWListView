a9a9dd954affc739d04b0e1088efc30e66375884
16022582 <16022582@myrp.edu.sg>
2018-04-23 12:39:34 +0800

    finished courses.java and coursesadapter.java

--- app/src/main/java/rp/tw_listview/CoursesAdapter.java
+++ app/src/main/java/rp/tw_listview/CoursesAdapter.java
@@ -1,4 +1,23 @@
 package rp.tw_listview;
 
-public class CoursesAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+
+
+public class CoursesAdapter extends ArrayAdapter<Courses> {
+    Context parent_content;
+    int layout_id;
+    ArrayList<Courses> aList;
+
+
+    public CoursesAdapter(Context context, int resource,
+                    ArrayList<Courses> objects){
+        super(context, resource, objects);
+
+        parent_content = context;
+        layout_id = resource;
+        aList = objects;
+
+    }
 }
--- app/src/main/java/rp/tw_listview/MainActivity.java
+++ app/src/main/java/rp/tw_listview/MainActivity.java
@@ -2,6 +2,9 @@ package rp.tw_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+
+        final ArrayList<Courses> newList;
+        CoursesAdapter caToDo;
+
+        newList = new ArrayList<Courses>();
+
+        caToDo = new CoursesAdapter(this, R.layout.activity_main, newList);
+
+        listView.setAdapter(caToDo);
+
+
+        final Courses item1 = new Courses("Year 1");
+        newList.add(item1);
+
+        final Courses item2 = new Courses("Year 2");
+        newList.add(item1);
+
+        final Courses item3 = new Courses("Year 3");
+        newList.add(item1);
     }
 }
